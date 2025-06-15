af1fb2d28159f62a6a19a751fa5dc5a48ff2e48b
Deep <dprajap2@gmail.com>
2025-06-15 00:00:10 -0400

    update heading text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="header">
-      <h1 id="header-title">Hi, Welcome.</h1>
+      <h1 id="header-title">Hi, Welcome!</h1>
       <h3 id="header-question">How can I help you today?</h3>
     </div>
   );
